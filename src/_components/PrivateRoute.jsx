7d7ef4544d408b596f665c1565c2ec9d54813f3d
aqxin <aqxin.cs@gmail.com>
2020-07-30 22:56:52 -0700

    Merge

--- src/_components/PrivateRoute.jsx
+++ src/_components/PrivateRoute.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
@@ -25,32 +24,4 @@ function PrivateRoute({ component: Component, roles, ...rest }) {
     );
 }
 
-=======
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-
-import { accountService } from '@/_services';
-
-function PrivateRoute({ component: Component, roles, ...rest }) {
-    return (
-        <Route {...rest} render={props => {
-            const user = accountService.userValue;
-            if (!user) {
-                // not logged in so redirect to login page with the return url
-                return <Redirect to={{ pathname: '/account/login', state: { from: props.location } }} />
-            }
-
-            // check if route is restricted by role
-            if (roles && roles.indexOf(user.role) === -1) {
-                // role not authorized so redirect to home page
-                return <Redirect to={{ pathname: '/'}} />
-            }
-
-            // authorized so return component
-            return <Component {...props} />
-        }} />
-    );
-}
-
->>>>>>> master
 export { PrivateRoute };
--- src/_components/index.js
+++ src/_components/index.js
@@ -1,7 +1,4 @@
 export * from './Alert';
 export * from './Nav';
 export * from './PrivateRoute';
-<<<<<<< HEAD
 export * from './Searchbar';
-=======
->>>>>>> master
