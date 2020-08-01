2bfc2ad0afaeee81f0e478c30254fdcbd523d291
workflow7861 <34591059+workflow7861@users.noreply.github.com>
2020-08-01 14:46:34 -0700

    Update Index.jsx
    included new pages

--- src/app/Index.jsx
+++ src/app/Index.jsx
@@ -3,11 +3,14 @@ import { Route, Switch, Redirect, useLocation } from 'react-router-dom';
 
 import { Role } from '@/_helpers';
 import { accountService } from '@/_services';
-import { Nav, PrivateRoute, Alert } from '@/_components';
+import { Nav, PrivateRoute, Alert, Searchbar } from '@/_components';
 import { Home } from '@/home';
 import { Profile } from '@/profile';
 import { Admin } from '@/admin';
 import { Account } from '@/account';
+import { Sets } from '@/sets'
+import { Study_Plan } from '@/study_plan';
+import { Metrics } from '@/metrics'
 
 function App() {
     const { pathname } = useLocation();  
@@ -21,11 +24,15 @@ function App() {
     return (
         <div className={'app-container' + (user && ' bg-light')}>
             <Nav />
+            {/* <Searchbar/> */}
             <Alert />
             <Switch>
                 <Redirect from="/:url*(/+)" to={pathname.slice(0, -1)} />
                 <PrivateRoute exact path="/" component={Home} />
                 <PrivateRoute path="/profile" component={Profile} />
+                <PrivateRoute path="/sets" component={Sets} />
+                <PrivateRoute path="/study_plan" component={Study_Plan} />
+                <PrivateRoute path="/metrics" component={Metrics} />
                 <PrivateRoute path="/admin" roles={[Role.Admin]} component={Admin} />
                 <Route path="/account" component={Account} />
                 <Redirect from="*" to="/" />
