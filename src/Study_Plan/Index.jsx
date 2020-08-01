d2247f9cde11b436459426367d7b78d14386b9eb
workflow7861 <34591059+workflow7861@users.noreply.github.com>
2020-08-01 14:45:47 -0700

    Add files via upload
    Added new pages (sets, metrics, and study plan)

---
+++ src/Study_Plan/Index.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { accountService } from '@/_services';
+import { Container, Row, Col } from 'react-bootstrap';
+
+function Study_Plan() {
+    const user = accountService.userValue;
+    
+    const welcome = {
+      fontSize: '2vw',
+      ///  paddingLeft: '50px',
+        /// paddingBottom: '50px'
+      }; 
+    const weekly = {
+      fontSize: '20px',
+      };
+    const border = {
+        border: '1px solid #C4C4C4',
+        width: '77vw',
+  
+      };
+      
+    const recent = {
+      top: '10vw',
+      };
+    /* const logo = {
+        position: 'relative',
+        right: '-1500px',
+        bottom: '470px'
+
+    }; */
+
+    /// Code returned when user reaches home page
+    return (
+        <div>
+              <Row>
+                <Col>
+                </Col>
+                 <Col style={welcome}>
+                Study Plan                    
+                <hr style={border} />
+                  </Col>
+                </Row>
+              <Row>
+              <Col></Col>
+              <Row> </Row>
+              <Row>
+              <Col style={weekly}> 
+              </Col>
+              </Row>
+              <Col></Col>
+              <Col>
+                 
+              </Col>
+              </Row>
+        </div>
+
+    );
+}
+
+export { Study_Plan };
