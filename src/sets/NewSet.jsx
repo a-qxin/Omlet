f97d8d193c33d233860c1491b59d82d2d909c542
aqxin <aqxin.cs@gmail.com>
2020-08-10 17:01:54 -0700

    Add NewSet.jsx

---
+++ src/sets/NewSet.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { accountService } from '@/_services';
+import { Container, Row, Col } from 'react-bootstrap';
+
+function NewSet() {
+    const user = accountService.userValue;
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export { NewSet };
