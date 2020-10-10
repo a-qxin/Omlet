65164be6e360a9aa4b17d72be8c864e54e17f747
aqxin <aqxin.cs@gmail.com>
2020-10-10 13:33:28 -0700

    Cleanup nav top style

--- src/_components/Nav.jsx
+++ src/_components/Nav.jsx
@@ -1,86 +1,80 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink, Route } from 'react-router-dom';
 import { Searchbar } from '@/_components';
+import { Container, Row, Col } from 'react-bootstrap';
 
-// import { Role } from '@/_helpers';
 import { accountService } from '@/_services';
 
-import { Container, Row, Col } from 'react-bootstrap';
-// import { Form, Button, FormControl } from 'react-bootstrap';
+const omlet = {
+  color: 'black',
+  fontSize: '40px',
+};
 
-    const omlet = {
-        color: 'black',
-        fontSize: '40px',
-        };
-
-    const welcome = {
-        fontSize: '2vw',
-        top: '50px',
-    }; 
-    const navlinks = {
-        color: 'black'
-    };
+// const welcome = {
+//   fontSize: '2vw',
+//   top: '50px',
+// };
+const navlinks = {
+  color: 'black'
+};
 
 function Nav() {
-    const [user, setUser] = useState({});
+  const [user, setUser] = useState({});
 
-    useEffect(() => {
-        const subscription = accountService.user.subscribe(x => setUser(x));
-        return subscription.unsubscribe;
-    }, []);
+  useEffect(() => {
+    const subscription = accountService.user.subscribe(x => setUser(x));
+    return subscription.unsubscribe;
+  }, []);
 
-    // only show nav when logged in
-    if (!user) return null;
+  // only show nav when logged in
+  if (!user) return null;
 
-    return (
+  return (
+    <div>
+      <nav className="navbar navbar-expand vertical-nav">
+        <Row style={{ height: "100%" }}>
+          <Col>
+            <NavLink exact to="/" className="nav-item nav-link" style={omlet}>
+              <img src='public/images/omlet-logo.png' style={{ width: "80px" }} />
+            </NavLink>
+            <NavLink exact to="/" className="nav-item nav-link" style={navlinks}>
+              {/* <img src='public/images/dash_icon.svg'></img> */}
+                Dashboard
+            </NavLink>
+            <NavLink exact to="/sets" className="nav-item nav-link" style={navlinks}>
+              {/* <img src='public/images/sets_icon.svg'></img> */}
+                Sets
+            </NavLink>
+            <NavLink exact to="/study_plan" className="nav-item nav-link" style={navlinks}>
+              {/* <img src='public/images/plan_icon.svg'></img> */}
+              Study Plan
+            </NavLink>
+            <NavLink exact to="/metrics" className="nav-item nav-link" style={navlinks}>
+              {/* <img src='public/images/metric_icon.svg'></img> */}
+              Metrics
+            </NavLink>
+            <a onClick={accountService.logout} className="nav-item nav-link" >Logout</a>
+          </Col>
+        </Row>
+      </nav>
+
+      <div style={{ margin: '0 0 30px 270px', padding: '0 0 18px 0', display:'flex', boxShadow: '0 5px 3px -4px #c4c4c4' }}>
+        <div style={{width:'100%', padding:'40px 0 0 100px'}}>
+          <Searchbar />
+        </div>
         <div>
-            <nav className="navbar navbar-expand vertical-nav">
-                <Row style={{height:"100%"}}>
-                    <Col>
-                        <NavLink exact to="/" className="nav-item nav-link" style={omlet}>
-                            <img src='public/images/omlet-logo.png' style={{ width: "90px" }} /> 
-                        </NavLink>
-                        <NavLink exact to="/" className="nav-item nav-link" style={navlinks}>
-                            {/* <img src='public/images/dash_icon.svg'></img> */}
-                            Dashboard
-                        </NavLink>
-                        <NavLink exact to="/sets" className="nav-item nav-link" style={navlinks}>
-                            {/* <img src='public/images/sets_icon.svg'></img> */}
-                            Sets
-                        </NavLink>
-                        <NavLink exact to="/study_plan" className="nav-item nav-link" style={navlinks}>
-                            {/* <img src='public/images/plan_icon.svg'></img> */}
-                            Study Plan
-                        </NavLink>
-                        <NavLink exact to="/metrics" className="nav-item nav-link" style={navlinks}>
-                            {/* <img src='public/images/metric_icon.svg'></img> */}
-                            Metrics
-                        </NavLink>
-                        <a onClick={accountService.logout} className="nav-item nav-link" >Logout</a>
-                    </Col>
-                </Row>
-            </nav>    
-            
-            <Col sm={1}/>
-                
-            <Row>
-                <Col sm={3}/>
-                <Col sm={5} style={{top:'20px'}}>
-                    <Searchbar/>
-                </Col>
-                <Col sm={3}/>
-                <Col>
-                    <NavLink to="/profile" className="nav-item nav-link pt-4" ><img src='public/images/omlet_mascot.png' style={{width:"40px", height:"40px"}}></img></NavLink>
-                </Col>
-            </Row>    
-                                  
-                    
-            {/* {user.role === Role.Admin &&
+          <NavLink to="/profile" className="nav-item nav-link pt-4" >
+            <img src='public/images/omlet-mascot.png' style={{ width: "42px", float: 'right', margin:'0 16px 8px 0' }}/>
+          </NavLink>
+        </div>
+      </div>
+
+      {/* {user.role === Role.Admin &&
                 <NavLink to="/admin" className="nav-item nav-link">Admin</NavLink>
             }
             <Route path="/admin" component={AdminNav} /> */}
-        </div>
-    );
+    </div>
+  );
 }
 
 // function AdminNav({ match }) {
