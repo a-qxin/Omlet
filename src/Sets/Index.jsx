e3672ef91d682dfa63733fbb5dc0f27b412285fe
aqxin <aqxin.cs@gmail.com>
2020-10-10 14:02:07 -0700

    Set color accents width fix

--- src/Sets/Index.jsx
+++ src/Sets/Index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { accountService } from '@/_services';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+// import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
 function Sets() {
   const user = accountService.userValue;
@@ -31,15 +31,34 @@ function Sets() {
     border: '1.5px dashed #000000',
   };
 
-  const setAccent = {
-    width: '18px',
+  const setAccentGreen = {
+    width: '14px',
     borderRadius:'20px 0 0 20px',
     height: '100%',
     background: '#2ee340'
   };
+  const setAccentRed = {
+    width: '14px',
+    borderRadius:'20px 0 0 20px',
+    height: '100%',
+    background: '#ff6767'
+  };
+  const setAccentBlue = {
+    width: '14px',
+    borderRadius:'20px 0 0 20px',
+    height: '100%',
+    background: '#00cabe'
+  };
+  const setAccentPurple = {
+    width: '14px',
+    borderRadius:'20px 0 0 20px',
+    height: '100%',
+    background: '#B073ff'
+  };
 
   const setContents = {
     margin: '30px 30px',
+    width: '100%',
   };
 
   const setTitle = {
@@ -47,7 +66,6 @@ function Sets() {
     fontSize: '26px',
     lineHeight: '30px',
     marginBottom:'8px'
-
   };
 
   const courseCode = {
@@ -69,7 +87,7 @@ function Sets() {
 
   return (
     <div>
-
+      {/* new set */}
       <div style={setContainer}>
         <div style={dottedBorder}>
           <div className="text" style={{ textAlign: 'center', margin: 'auto', padding: '80px 0' }}>
@@ -80,12 +98,39 @@ function Sets() {
           </div>
         </div>
 
+        {/* user's sets to map */}
         <div style={set}>
-          <div style={setAccent}></div>
+          <div style={setAccentGreen}></div>
           <div style={setContents}>
             <div>
               <p style={setTitle}>Defence Against the Dark Arts</p>
               <p style={courseCode}>DADA - 101</p>
+              <hr style={setHr} />
+              <p style={numTerms}>15 Terms</p>
+            </div>
+            
+          </div>
+        </div>
+
+        <div style={set}>
+          <div style={setAccentPurple}></div>
+          <div style={setContents}>
+            <div>
+              <p style={setTitle}>Potions</p>
+              <p style={courseCode}>PTNS - 101</p>
+              <hr style={setHr} />
+              <p style={numTerms}>8 Terms</p>
+            </div>
+            
+          </div>
+        </div>
+
+        <div style={set}>
+          <div style={setAccentBlue}></div>
+          <div style={setContents}>
+            <div>
+              <p style={setTitle}>History of Magic</p>
+              <p style={courseCode}>HOM - 101</p>
               <hr style={setHr} />
               <p style={numTerms}>5 Terms</p>
             </div>
@@ -94,15 +139,16 @@ function Sets() {
         </div>
 
         <div style={set}>
-        </div>
-        <div style={set}>
-        </div>
-        <div style={set}>
-        </div>
-        <div style={set}>
-        </div><div style={set}>
-        </div><div style={set}>
-        </div><div style={set}>
+          <div style={setAccentRed}></div>
+          <div style={setContents}>
+            <div>
+              <p style={setTitle}>Transfiguration</p>
+              <p style={courseCode}>TNFG - 101</p>
+              <hr style={setHr} />
+              <p style={numTerms}>13 Terms</p>
+            </div>
+            
+          </div>
         </div>
 
       </div>
