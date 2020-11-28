4bd24fd23591c495b46d0cd01ca1d5ea920784f0
aqxin <aqxin.cs@gmail.com>
2020-11-28 13:38:58 -0800

    Format ResetPassword.jsx

--- src/account/ResetPassword.jsx
+++ src/account/ResetPassword.jsx
@@ -7,105 +7,105 @@ import * as Yup from 'yup';
 import { accountService, alertService } from '@/_services';
 
 function ResetPassword({ history }) {
-    const TokenStatus = {
-        Validating: 'Validating',
-        Valid: 'Valid',
-        Invalid: 'Invalid'
-    }
-    
-    const [token, setToken] = useState(null);
-    const [tokenStatus, setTokenStatus] = useState(TokenStatus.Validating);
+	const TokenStatus = {
+		Validating: 'Validating',
+		Valid: 'Valid',
+		Invalid: 'Invalid'
+	}
 
-    useEffect(() => {
-        const { token } = queryString.parse(location.search);
+	const [token, setToken] = useState(null);
+	const [tokenStatus, setTokenStatus] = useState(TokenStatus.Validating);
 
-        // remove token from url to prevent http referer leakage
-        history.replace(location.pathname);
+	useEffect(() => {
+		const { token } = queryString.parse(location.search);
 
-        accountService.validateResetToken(token)
-            .then(() => {
-                setToken(token);
-                setTokenStatus(TokenStatus.Valid);
-            })
-            .catch(() => {
-                setTokenStatus(TokenStatus.Invalid);
-            });
-    }, []);
+		// remove token from url to prevent http referer leakage
+		history.replace(location.pathname);
 
-    function getForm() {
-        const initialValues = {
-            password: '',
-            confirmPassword: ''
-        };
+		accountService.validateResetToken(token)
+			.then(() => {
+				setToken(token);
+				setTokenStatus(TokenStatus.Valid);
+			})
+			.catch(() => {
+				setTokenStatus(TokenStatus.Invalid);
+			});
+	}, []);
 
-        const validationSchema = Yup.object().shape({
-            password: Yup.string()
-                .min(6, 'Password must be at least 6 characters')
-                .required('Password is required'),
-            confirmPassword: Yup.string()
-                .oneOf([Yup.ref('password'), null], 'Passwords must match')
-                .required('Confirm Password is required'),
-        });
+	function getForm() {
+		const initialValues = {
+			password: '',
+			confirmPassword: ''
+		};
 
-        function onSubmit({ password, confirmPassword }, { setSubmitting }) {
-            alertService.clear();
-            accountService.resetPassword({ token, password, confirmPassword })
-                .then(() => {
-                    alertService.success('Password reset successful, you can now login', { keepAfterRouteChange: true });
-                    history.push('login');
-                })
-                .catch(error => {
-                    setSubmitting(false);
-                    alertService.error(error);
-                });
-        }
+		const validationSchema = Yup.object().shape({
+			password: Yup.string()
+				.min(6, 'Password must be at least 6 characters')
+				.required('Password is required'),
+			confirmPassword: Yup.string()
+				.oneOf([Yup.ref('password'), null], 'Passwords must match')
+				.required('Confirm Password is required'),
+		});
 
-        return (
-            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
-                {({ errors, touched, isSubmitting }) => (
-                    <Form>
-                        <div className="form-group">
-                            <label>Password</label>
-                            <Field name="password" type="password" className={'form-control' + (errors.password && touched.password ? ' is-invalid' : '')} />
-                            <ErrorMessage name="password" component="div" className="invalid-feedback" />
-                        </div>
-                        <div className="form-group">
-                            <label>Confirm Password</label>
-                            <Field name="confirmPassword" type="password" className={'form-control' + (errors.confirmPassword && touched.confirmPassword ? ' is-invalid' : '')} />
-                            <ErrorMessage name="confirmPassword" component="div" className="invalid-feedback" />
-                        </div>
-                        <div className="form-row">
-                            <div className="form-group col">
-                                <button type="submit" disabled={isSubmitting} className="btn btn-primary">
-                                    {isSubmitting && <span className="spinner-border spinner-border-sm mr-1"></span>}
-                                    Reset Password
-                                </button>
-                                <Link to="/login" className="btn btn-link">Cancel</Link>
-                            </div>
-                        </div>
-                    </Form>
-                )}
-            </Formik>
-        );
-    }
+		function onSubmit({ password, confirmPassword }, { setSubmitting }) {
+			alertService.clear();
+			accountService.resetPassword({ token, password, confirmPassword })
+				.then(() => {
+					alertService.success('Password reset successful, you can now login', { keepAfterRouteChange: true });
+					history.push('login');
+				})
+				.catch(error => {
+					setSubmitting(false);
+					alertService.error(error);
+				});
+		}
 
-    function getBody() {
-        switch (tokenStatus) {
-            case TokenStatus.Valid:
-                return getForm();
-            case TokenStatus.Invalid:
-                return <div>Token validation failed, if the token has expired you can get a new one at the <Link to="forgot-password">forgot password</Link> page.</div>;
-            case TokenStatus.Validating:
-                return <div>Validating token...</div>;
-        }
-    }
+		return (
+			<Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+				{({ errors, touched, isSubmitting }) => (
+					<Form>
+						<div className="form-group">
+							<label>Password</label>
+							<Field name="password" type="password" className={'form-control' + (errors.password && touched.password ? ' is-invalid' : '')} />
+							<ErrorMessage name="password" component="div" className="invalid-feedback" />
+						</div>
+						<div className="form-group">
+							<label>Confirm Password</label>
+							<Field name="confirmPassword" type="password" className={'form-control' + (errors.confirmPassword && touched.confirmPassword ? ' is-invalid' : '')} />
+							<ErrorMessage name="confirmPassword" component="div" className="invalid-feedback" />
+						</div>
+						<div className="form-row">
+							<div className="form-group col">
+								<button type="submit" disabled={isSubmitting} className="btn btn-primary">
+									{isSubmitting && <span className="spinner-border spinner-border-sm mr-1"></span>}
+									Reset Password
+								</button>
+								<Link to="/login" className="btn btn-link">Cancel</Link>
+							</div>
+						</div>
+					</Form>
+				)}
+			</Formik>
+		);
+	}
 
-    return (
-        <div>
-            <h3 className="card-header">Reset Password</h3>
-            <div className="card-body">{getBody()}</div>
-        </div>
-    )
+	function getBody() {
+		switch (tokenStatus) {
+			case TokenStatus.Valid:
+				return getForm();
+			case TokenStatus.Invalid:
+				return <div>Token validation failed, if the token has expired you can get a new one at the <Link to="forgot-password">forgot password</Link> page.</div>;
+			case TokenStatus.Validating:
+				return <div>Validating token...</div>;
+		}
+	}
+
+	return (
+		<div>
+			<h3 className="card-header">Reset Password</h3>
+			<div className="card-body">{getBody()}</div>
+		</div>
+	)
 }
 
 export { ResetPassword }; 
