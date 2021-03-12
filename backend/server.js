011241159a8d616df560cc2bfa0d3c32b980fe57
a-qxin <aqxin.cs@gmail.com>
2021-03-11 18:37:50 -0800

    Clean up mongodb connection in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 ﻿require('rootpath')();
 const express = require('express');
-const app = express();
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const errorHandler = require('_middleware/error-handler');
+// const mongoose = require('mongoose');
 // const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+const app = express();
 
 // bodyParser not req in new Express
 // app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,7 +18,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
-require('dotenv').config();
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true });
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//     console.log("MongoDB database connection established successfully");
+// })
 
 // allow cors requests from any origin and with credentials
 app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
